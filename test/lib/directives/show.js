ea3d3a15902a9d5db1ffb2b03a5ebaf6d2bfc0ca
Jesse Ditson <jesse.ditson@gmail.com>
2014-11-15 19:27:05 -0800

    add show tests

---
+++ test/lib/directives/show.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+
+var eggs = require('../../../lib/eggs');
+var utils = require('../../utils');
+
+describe('eggs show directive',function(){
+  var $;
+  var e;
+  var vm;
+
+  before(function(){
+    $ = utils.loadHTML('<div><div e-html="html"><div id="content"><div id="showme" e-show="show"></div>');
+    e = eggs($,{selector : '#content'});
+    vm = {};
+    e.bind(vm);
+  });
+
+  it('should hide an element if the value is falsey',function(done){
+    vm.show = false;
+    setTimeout(function(){
+      assert.equal($('#showme').css('display'),'none');
+      done();
+    },utils.updateTimeout);
+  });
+
+  it('should show an element if the value is truthy',function(done){
+    vm.show = true;
+    setTimeout(function(){
+      assert(!$('#showme').css('display'));
+      done();
+    },utils.updateTimeout);
+  });
+});
